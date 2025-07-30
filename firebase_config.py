27ce9670328e5aa0bdb26f0c22d07f7207ad80d9
rishichhadva <rishichhadva31749@gmail.com>
2025-07-30 19:41:31 +0530

    added firebase_config.py

---
+++ firebase_config.py
@@ -0,0 +1,11 @@
+
+firebase_config = {
+    "apiKey": "AIzaSyDbVxnX-JyM6LrfZn2Sj_IEZRC1GYuP-sk",
+    "authDomain": "personal-finance-tracker-rishi.firebaseapp.com",
+    "projectId": "personal-finance-tracker-rishi",
+    "storageBucket": "personal-finance-tracker-rishi.firebasestorage.app",
+    "messagingSenderId": "815652739645",
+    "appId": "1:815652739645:web:1d35eaac56dd0577dae854",
+    "measurementId": "G-7H1Y6B8F0V",
+    "databaseURL": ""
+}
