763e493b1951a03595c5780c5a9ed34c8ef613ef
rishichhadva <rishichhadva31749@gmail.com>
2025-07-30 21:43:23 +0530

    modified app.py

--- firebase_config.py
+++ firebase_config.py
@@ -7,5 +7,5 @@ firebase_config = {
     "messagingSenderId": "815652739645",
     "appId": "1:815652739645:web:1d35eaac56dd0577dae854",
     "measurementId": "G-7H1Y6B8F0V",
-    "databaseURL": "https://personal-finance-tracker-rishi-default-rtdb.firebaseio.com/"
+    "databaseURL": "https://personal-finance-tracker-rishi-default-rtdb.firebaseio.com"
 }
