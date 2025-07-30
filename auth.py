7c4adbc0b589af5d28c5cb151b77e62e800857c8
rishichhadva <rishichhadva31749@gmail.com>
2025-07-30 20:09:37 +0530

    Add databaseURL to firebase config

--- auth.py
+++ auth.py
@@ -7,7 +7,8 @@ firebaseConfig = {
     "storageBucket": "personal-finance-tracker-rishi.appspot.com",
     "messagingSenderId": "815652739645",
     "appId": "1:815652739645:web:1d35eaac56dd0577dae854",
-    "measurementId": "G-7H1Y6B8F0V"
+    "measurementId": "G-7H1Y6B8F0V",
+    "databaseURL": "https://personal-finance-tracker-rishi-default-rtdb.firebaseio.com"
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
